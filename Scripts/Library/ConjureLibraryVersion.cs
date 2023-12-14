981dc85eca7c2ddb0fb514ea03d91ddb5c9245f0
vincentGingras4 <vincent.gingras.4@ens.etsmtl.ca>
2023-12-14 16:25:14 -0500

    Cleaning folder

--- Scripts/Library/ConjureLibraryVersion.cs
+++
@@ -1,7 +0,0 @@
-using ConjureOS.Library;
-
-public static class ConjureLibraryVersion
-{
-    public static ConjureVersion Version =
-        new ConjureVersion(1, 0, 0); // Conjure Unity library version
-}
